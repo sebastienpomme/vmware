0e844ba0fc7831c8e28d97224e010cca7728e9ee
sebastienpomme <47449442+sebastienpomme@users.noreply.github.com>
2019-02-08 19:40:45 +0100

    Update Connect_Vcenter.ps1

--- Connect_Vcenter.ps1
+++ Connect_Vcenter.ps1
@@ -3,7 +3,7 @@
 ################     Find more on my GitHub     	   ################ 
 
 # How to use the command:
-# .\Connect_Vcenter.ps1 -vcserver name_of_vcenter -user username -password password -logspath C:\connect_vcenter.txt
+# .\Connect_Vcenter.ps1 -vcserver name/ip_of_vcenter -user username -password password -logspath C:\connect_vcenter.txt
 # WARNING : the powercli doesn't not support some specials characters on password parsing input. If needed, enclose the entire string in single-quotes (in that case, the password should not contain any single-quote)
 
 ################			Script			################
